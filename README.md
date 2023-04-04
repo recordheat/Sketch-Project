b0998f4f3d07b8f46a749f2d16751648729700ee
Jacob Xu <113264182+recordheat@users.noreply.github.com>
2023-04-03 19:16:34 -0700

    Final Commit
    Final style phasing process. Made the buttons look cooler and have a sound effect on click.  Also made the code look a little bit more cleaner as well.

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
-* Second project from The Odin Project cirriculum. What this project focuses on is the use of recalling Javascript fundamentals while also implementing HTML/CSS 
-foundations from the course. 
-* Googling things and using ChatGPT as a teacher to help is what will make this project go smoothly. 
+Etch A Sketch Project
+* This is the second project from The Odin Project cirriculum. What this project focuses on is the use of recalling Javascript fundamentals while also implementing HTML/CSS 
+foundations from the course. Googling things and using ChatGPT as a teacher to help is what will make this project go smoothly as I have already done a project
+from their course.
+
+- To be able to sketch anything on this website, hover over the gridbox and draw whatever you would like.
--- script.js
+++ script.js
@@ -1,11 +1,12 @@
 
-// Sketch functions to select / call
+// Sketch function for gridbox
 function sketch () {
 const container = document.querySelector('.container');
 const gridBox = document.createElement('div');
 const clearBtn = document.querySelector('#clear');
 const rainbowMode = document.querySelector('#random-colors');
-
+const defaultColor = document.querySelector('#default');
+const audio = document.querySelector('#mouse-click');
 gridBox.setAttribute('id','gridBox');
 container.appendChild(gridBox);
 
@@ -23,12 +24,64 @@ row.appendChild(square);
 square.addEventListener('mouseover', function() {
     square.style.backgroundColor = 'black';
     });
-
 const squares = document.querySelectorAll('.square');
+// Event listener for Clear Button
+clearBtn.addEventListener('click', () => {
+    clearBtn.style.transform = "scale(1.1)";
+    audio.playbackRate = 1.5;
+    audio.currentTime = 0;
+    audio.play();
+});
 
+clearBtn.addEventListener('mouseout', () => {
+    clearBtn.style.transform = "scale(1.0)";
+});
+
+clearBtn.addEventListener('mouseover', () => {
+    clearBtn.style.transform = "scale(1.1)";
+});
 // Button listener event that clears/resets the gridbox
 clearBtn.addEventListener('click', function () {
-squares.forEach(square => square.style.backgroundColor = 'lightsalmon');
+squares.forEach(square => square.style.backgroundColor = 'white');
+gridBox.style.borderBottom = 'solid black';
+});
+// Event listener for Default Color button
+defaultColor.addEventListener('click', () => {
+    defaultColor.style.transform = "scale(1.1)";
+    audio.playbackRate = 1.5;
+    audio.currentTime = 0;
+    audio.play();
+});
+
+defaultColor.addEventListener('mouseover', () => {
+    defaultColor.style.transform = "scale(1.1)";
+});
+
+defaultColor.addEventListener('mouseout', () => {
+    defaultColor.style.transform = "scale(1.0)";
+});
+// Default button event listener that chooses a black color to hover over gridbox
+    defaultColor.addEventListener('click', function () {
+    squares.forEach(function(square) {
+    square.addEventListener('mouseover', function () {
+    square.style.backgroundColor = 'black';
+        });
+    });
+});
+// Event listeners for Rainbow Mode button
+rainbowMode.addEventListener('click', () => {
+    rainbowMode.style.transform = "scale(1.1)";
+    audio.playbackRate = 1.5;
+    audio.currentTime = 0;
+    audio.play();
+});
+
+rainbowMode.addEventListener('mouseover', () => {
+    rainbowMode.style.transform = "scale(1.1)";
+});
+
+rainbowMode.addEventListener('mouseout', () => {
+    rainbowMode.style.transform = "scale(1.0)";
 });
 // Selects a button to select different colors to cover the grid
 rainbowMode.addEventListener('click', function () {
@@ -38,10 +91,9 @@ squares.forEach(function(square) {
     const g = Math.floor(Math.random() * 256);
     const b = Math.floor(Math.random() * 256);
     square.style.backgroundColor = `rgb(${r}, ${g}, ${b})`;
+        });
     });
 });
-});
-
          }
      }
 }
